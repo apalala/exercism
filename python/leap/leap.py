91b82273af975e2001603a315d43b57916d23129
Juancarlo Añez <apalala@gmail.com>
2017-01-24 13:22:55 -0400

    Leap: simplify, testing for 400 divisibility first

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -5,9 +5,7 @@ def _evenly_divisible(n, m):
 
 
 def is_leap_year(year):
-    if not _evenly_divisible(year, 4):
-        return False
-    elif _evenly_divisible(year, 100):
-        return _evenly_divisible(year, 400)
-    else:
-        return True
+    return (
+        _evenly_divisible(year, 400) or
+        _evenly_divisible(year, 4) and not _evenly_divisible(year, 100)
+    )
