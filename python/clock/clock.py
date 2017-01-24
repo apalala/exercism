94d19ab67ee3e226bca4922ae0d2e2848979873a
Juancarlo Añez <apalala@gmail.com>
2017-01-24 15:04:44 -0400

    Clock: simplify by only keeping track of the minute of the day

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -2,17 +2,19 @@
 
 class Clock():
     def __init__(self, hours, minutes):
-        self.minutes = minutes % 60
-        self.hours = (hours + minutes // 60) % 24
+        self.minute_of_the_day = (hours * 60 + minutes) % (24 * 60)
 
     def add(self, minutes):
-        return Clock(self.hours, self.minutes + minutes)
+        return Clock(0, self.minute_of_the_day + minutes)
+
+    def _hours_and_minutes(self):
+        return (self.minute_of_the_day // 60, self.minute_of_the_day % 60)
 
     def __hash__(self):
-        return self.hours ^ self.minutes
+        return hash(self.minute_of_the_day)
 
     def __eq__(self, other):
-        return self.hours == other.hours and self.minutes == other.minutes
+        return self.minute_of_the_day == other.minute_of_the_day
 
     def __str__(self):
-        return '%02d:%02d' % (self.hours, self.minutes)
+        return '%02d:%02d' % self._hours_and_minutes()
