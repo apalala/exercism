336e2f9c3adf5ebee16413c3f764d12ef698f0f8
Juancarlo Añez <apalala@gmail.com>
2017-01-24 15:13:39 -0400

    Leap: function names should be as short as readable/understandable

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,11 +1,11 @@
 
 
-def _evenly_divisible(n, m):
+def _divisible(n, m):
     return not n % m
 
 
 def is_leap_year(year):
     return (
-        _evenly_divisible(year, 400) or
-        _evenly_divisible(year, 4) and not _evenly_divisible(year, 100)
+        _divisible(year, 400) or
+        _divisible(year, 4) and not _divisible(year, 100)
     )
