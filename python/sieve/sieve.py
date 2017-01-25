9bb0ea8f76958d4c314a2a7136751da57f94374c
Juancarlo Añez <apalala@gmail.com>
2017-01-25 12:49:06 -0400

    Solution to the Sieve problem

---
+++ python/sieve/sieve.py
@@ -0,0 +1,12 @@
+
+
+def sieve(limit):
+    numbers = list(range(2, limit + 1))
+    not_prime = set()
+
+    for n in numbers:
+        if n in not_prime:
+            continue
+        not_prime.update(n * k for k in range(2, (limit + n) // n))
+
+    return list(sorted(set(numbers) - not_prime))
