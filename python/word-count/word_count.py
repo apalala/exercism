d4312908f9041d5d60787c0039eab5951b5fb297
Juancarlo Añez <apalala@gmail.com>
2017-01-25 07:57:12 -0400

    Word Count: simplified to one-liner after studying other solutions

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,11 +3,4 @@ from collections import Counter
 
 
 def word_count(text):
-    filtered_text = text.replace('_', ' ')
-    words = re.split('\W+', filtered_text.lower())
-
-    counts = Counter(words)
-    if '' in counts:
-        del counts['']
-
-    return counts
+    return Counter(re.findall('[^\W_]+', text.lower()))
