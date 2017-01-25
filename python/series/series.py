cb49dbf5aecf567182fd9ff9c1e8b10915f570be
Juancarlo Añez <apalala@gmail.com>
2017-01-25 09:31:52 -0400

    Series: refactoring

--- python/series/series.py
+++ python/series/series.py
@@ -5,7 +5,6 @@ def slices(series, n):
     if not n or n > len(series):
         raise ValueError('Length %d is out of range' % n)
 
-    return [
-        [int(i) for i in s]
-        for s in zip(*[islice(series, i, len(series)) for i in range(n)])
-    ]
+    substrings = zip(*[islice(series, i, len(series)) for i in range(n)])
+
+    return [[int(c) for c in s] for s in substrings]
