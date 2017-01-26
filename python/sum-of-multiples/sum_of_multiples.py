a316583c8d91f8a1ef4b602d642dd3ba77962d0b
Juancarlo Añez <apalala@gmail.com>
2017-01-26 07:38:56 -0400

    Sum of multiples: clarity and performance as suggested by jgomo3

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -2,6 +2,8 @@
 
 def sum_of_multiples(limit, numbers):
     multiples = set(n for n in numbers if n < limit)
+
     for n in multiples - {0}:
-        multiples.update(i * n  for i in range(2, (limit + n - 1) // n))
+        multiples.update(range(n, limit, n))
+
     return sum(multiples)
