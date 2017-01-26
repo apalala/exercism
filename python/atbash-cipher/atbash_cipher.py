6fa03b855f824e09a571f554ef223a10df8fbef3
Juancarlo Añez <apalala@gmail.com>
2017-01-26 07:22:56 -0400

    Atbash: minor optimization, as suggested by jgomo3

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -6,17 +6,14 @@ CODE = dict(zip(string.ascii_lowercase, reversed(string.ascii_lowercase)))
 
 
 def _flip(c):
-    if c.isalpha():
-        return CODE[c]
-    elif c.isdigit():
-        return c
-    else:
-        return ''
+    return CODE.get(c, c) if c.isalnum() else ''
 
 
 def encode(text):
     ciphertext = ''.join(_flip(c) for c in text.lower())
+
     chunks_of_5 = zip_longest(*[iter(ciphertext)] * 5, fillvalue='')
+
     return ' '.join(''.join(chunk) for chunk in chunks_of_5)
 
 
