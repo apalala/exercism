035a9ae434288f55d92466f89b37b732071d27c4
Juancarlo Añez <apalala@gmail.com>
2017-01-25 20:24:11 -0400

    Anagram: simplifications from team talk

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -3,10 +3,10 @@
 def _is_anagram(word, candidate):
     word, candidate = word.lower(), candidate.lower()
 
-    if word == candidate:
-        return False
-
-    return list(sorted(word)) == list(sorted(candidate))
+    return (
+        word != candidate and
+        sorted(word) == sorted(candidate)
+    )
 
 
 def detect_anagrams(word, candidates):
