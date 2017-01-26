c177a9dde059bf6d394a6ff3bc120c32a77fb33d
Juancarlo Añez <apalala@gmail.com>
2017-01-26 07:59:07 -0400

    Acronyms: added support for Unicode

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,6 +1,22 @@
-import re
 
 
 def abbreviate(phrase):
-    words = re.findall('[A-Z]+[a-z]*|[a-z]+', phrase)
-    return ''.join(w[0].upper() for w in words)
+    return ''.join(w[0].upper() for w in _words(phrase))
+
+
+def _words(phrase):
+    words = []
+    i = 0
+    while i < len(phrase):
+        if not phrase[i].isalpha():
+            i += 1
+        else:
+            j = i
+            while j < len(phrase) and phrase[j].isupper():
+                j += 1
+            while j < len(phrase) and phrase[j].islower():
+                j += 1
+            words.append(phrase[i:j])
+            i = j
+
+    return words
