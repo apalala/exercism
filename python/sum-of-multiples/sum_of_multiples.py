d645a8051e1144f10ee428543a1ba5b23ed8c529
Juancarlo Añez <apalala@gmail.com>
2017-01-26 10:21:48 -0400

    Sum of Multiples: simplifications after discussion and study

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,9 +1,4 @@
 
 
 def sum_of_multiples(limit, numbers):
-    multiples = set(n for n in numbers if n < limit)
-
-    for n in multiples - {0}:
-        multiples.update(range(n, limit, n))
-
-    return sum(multiples)
+    return sum({i for n in numbers if n for i in range(n, limit, n)})
