03668beb54f83b8bad6bbca6b58d8a703cf2811f
Juancarlo Añez <apalala@gmail.com>
2017-01-26 10:24:35 -0400

    Sum of Multiples: format vertically

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,4 +1,10 @@
 
 
 def sum_of_multiples(limit, numbers):
-    return sum({i for n in numbers if n for i in range(n, limit, n)})
+    return sum(
+        {
+            i
+            for n in numbers if n
+            for i in range(n, limit, n)
+        }
+    )
