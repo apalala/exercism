29c0fd1013d9c4fd2507e47ce4124a3b11d7e5bf
Juancarlo Añez <apalala@gmail.com>
2017-01-26 20:27:23 -0400

    Sieve: refactoring

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -2,11 +2,11 @@
 
 def sieve(limit):
     numbers = list(range(2, limit + 1))
-    not_prime = set()
 
+    not_prime = set()
     for n in numbers:
         if n in not_prime:
             continue
-        not_prime.update(n * k for k in range(2, (limit + n) // n))
+        not_prime.update(range(2 * n, limit + 1, n))
 
     return sorted(set(numbers) - not_prime)
