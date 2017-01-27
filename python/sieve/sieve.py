69ebedc6b56dae8c42b9249e6822b25f141d0a2a
Juancarlo Añez <apalala@gmail.com>
2017-01-27 08:11:29 -0400

    Siever: optimize for odd numbers

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,12 +1,12 @@
 
 
 def sieve(limit):
-    numbers = list(range(2, limit + 1))
+    numbers = [2] + list(range(3, limit + 1, 2))
 
     not_prime = set()
     for n in numbers:
         if n in not_prime:
             continue
-        not_prime.update(range(2 * n, limit + 1, n))
+        not_prime.update(range(3 * n, limit + 1, 2 * n))
 
     return sorted(set(numbers) - not_prime)
