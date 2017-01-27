1a7c891dc48f001fdee1856bc1fb539f8611c948
Juancarlo Añez <apalala@gmail.com>
2017-01-27 17:55:30 -0400

    Sublist: Optimized with explicit for looks and shortcutting comparisons (http://stackoverflow.com/a/12576755/545637)

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -17,18 +17,25 @@ UNEQUAL = SubListType.UNEQUAL
 def check_lists(alist, blist):
     if alist == blist:
         return EQUAL
-    elif _has_sublist(alist, blist):
-        return SUBLIST
     elif _has_sublist(blist, alist):
+        return SUBLIST
+    elif _has_sublist(alist, blist):
         return SUPERLIST
     else:
         return UNEQUAL
 
 
-def _has_sublist(sub, alist):
-    for i in range(1 + len(alist) - len(sub)):
-        for j, s in enumerate(sub):
-            if s != alist[i + j]:
-                break
-        else:
-            return True
+def _has_sublist(alist, sub):
+    # See:
+    #   http://stackoverflow.com/a/12576755/545637
+    if not sub:
+        return True
+    elif len(sub) > len(alist):
+        return False
+    else:
+        for i in range(1 + len(alist) - len(sub)):
+            if sub[0] != alist[i]:
+                continue
+            if sub == alist[i:i + len(sub)]:
+                return True
+        return False
