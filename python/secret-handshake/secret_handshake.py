393dc3eba78b31c34a21010a05272dc2c825a719
Juancarlo Añez <apalala@gmail.com>
2017-01-29 21:46:09 -0400

    Solution to Secret Handshake problem

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -6,31 +6,38 @@ CODE = [
     'close your eyes',
     'jump',
 ]
-
-REVERSE_CODE = {name: i for i, name in enumerate(CODE)}
+REVERSE = len(CODE)
 
 
 def handshake(code):
-    if isinstance(code, str):
-        if not all(d in '01' for d in code):
-            return []
-        code = int(code, 2)
-    if code < 0:
+    try:
+        code = _as_int_code(code)
+    except ValueError:
         return []
 
-    names = []
-    for i, name in enumerate(CODE):
-        if (1 << i) & code:
-            names.append(name)
-    if (1 << 4) & code:
-        names = names.reverse()
-    return names
+    codes = [i for i in range(len(CODE)) if (1 << i) & code]
+    if (1 << REVERSE) & code:
+        codes.reverse()
+
+    return [CODE[c] for c in codes]
 
 
 def code(words):
-    value = 0
-    for name in words:
-        value |= REVERSE_CODE.get(name, 0)
-    return str(value)
+    try:
+        codes = [CODE.index(w) for w in words]
+    except ValueError:
+        return '0'
+
+    if codes != sorted(codes):
+        codes.append(len(CODE))
+
+    return '{:b}'.format(sum(1 << c for c in codes))
 
 
+def _as_int_code(code):
+    if isinstance(code, str):
+        return int(code, 2)
+    elif code < 0:
+        raise ValueError('Code is negative')
+    else:
+        return code
