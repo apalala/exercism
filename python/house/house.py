386f70a218fa39179119c18e036a4244c6770300
Juancarlo Añez <apalala@gmail.com>
2017-01-31 17:49:09 -0400

    Solution to the House problem

---
+++ python/house/house.py
@@ -0,0 +1,99 @@
+VERSES = [
+    'This is the house that Jack built.',
+
+    'This is the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the maiden all forlorn\n'
+    'that milked the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the man all tattered and torn\n'
+    'that kissed the maiden all forlorn\n'
+    'that milked the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the priest all shaven and shorn\n'
+    'that married the man all tattered and torn\n'
+    'that kissed the maiden all forlorn\n'
+    'that milked the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the rooster that crowed in the morn\n'
+    'that woke the priest all shaven and shorn\n'
+    'that married the man all tattered and torn\n'
+    'that kissed the maiden all forlorn\n'
+    'that milked the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the farmer sowing his corn\n'
+    'that kept the rooster that crowed in the morn\n'
+    'that woke the priest all shaven and shorn\n'
+    'that married the man all tattered and torn\n'
+    'that kissed the maiden all forlorn\n'
+    'that milked the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.',
+
+    'This is the horse and the hound and the horn\n'
+    'that belonged to the farmer sowing his corn\n'
+    'that kept the rooster that crowed in the morn\n'
+    'that woke the priest all shaven and shorn\n'
+    'that married the man all tattered and torn\n'
+    'that kissed the maiden all forlorn\n'
+    'that milked the cow with the crumpled horn\n'
+    'that tossed the dog\n'
+    'that worried the cat\n'
+    'that killed the rat\n'
+    'that ate the malt\n'
+    'that lay in the house that Jack built.'
+]
+
+
+def verse(n):
+    return VERSES[n]
+
+
+def rhyme():
+    return '\n\n'.join(VERSES)
