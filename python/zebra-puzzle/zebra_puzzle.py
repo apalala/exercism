b033e5eb3306570edfe3d516e0eb891ea0b17080
Juancarlo Añez <apalala@gmail.com>
2017-02-02 13:42:53 -0400

    Zebra Puzzle: another failed attempt

--- python/zebra-puzzle/zebra_puzzle.py
+++ python/zebra-puzzle/zebra_puzzle.py
@@ -1,14 +1,17 @@
-from enum import Enum
+from enum import IntEnum
 from itertools import count
 
-class R(Enum):
+
+class R(IntEnum):
     NUMBER = 0
-    NEXT = 1
-    WHO = 2
-    COLOR = 3
-    OWNS = 4
-    DRINKS = 5
-    SMOKES = 6
+    WHO = 1
+    COLOR = 2
+    OWNS = 3
+    DRINKS = 4
+    SMOKES = 5
+
+    RIGHT = 6
+    NEXT = 7
 
 
 def _error(a, b):
@@ -17,188 +20,196 @@ def _error(a, b):
 
 class Var():
     counter = count()
-    def __init__(self):
-        self._id = next(self.counter)
-        self._value = None
 
+    def __init__(self, value=None):
+        self.id = next(self.counter)
+        self._value = value
+
+    @property
     def value(self):
         if self._value is None:
             return None
         elif isinstance(self._value, Var):
-            return self._value.value()
+            return self._value.value
         else:
             return self._value
 
-    def set(self, value):
-        if self._value is None:
-            print(self)
-            self._value = value
-        elif isinstance(self._value, Var):
-            self._value.set(value)
-        else:
-            _error(self, value)
+    @value.setter
+    def value(self, value):
+        print('set', self, self._value, value)
+        assert self.value is None
+        self._value = value
 
-    def unify(self, other):
-        if self == other:
-            pass
-        elif self.value() is None:
-            self.set(other)
-        elif isinstance(other, Var):
-            other.set(self)
-        else:
-            _error(self, other)
+    def __eq__(self, other):
+        return (
+            super().__eq__(other) or
+            self._value == other or
+            self._value == other.value
+        )
+
+    def __lt__(self, other):
+        return self.id < other.id
 
     def __repr__(self):
-        return '%d: %s' % (self._id, str(self._value))
+        if self._value is not None:
+            return 'v%d->%s' % (self.id, repr(self._value))
+        else:
+            return 'v%d' % self.id
 
 
-def var():
-    return Var()
+def var(value=None):
+    return Var(value=value)
+
+
+def unify_atom(a, b):
+    if a == b:
+        pass
+    elif isinstance(a, Var) and a.value is None:
+        a.value = b
+    elif isinstance(b, Var) and b.value is None:
+        b.value = a
+    else:
+        return a == b
+    return True
 
 
 def unify(a, b):
-    if isinstance(a, Var):
-        a.unify(b)
-    elif isinstance(b, Var):
-        b.unify(a)
-    elif isinstance(a, tuple) and isinstance(b, tuple) and a[1] == b[1]:
-        for x, y in zip(a, b):
-            unify(x, y)
-        print(a, b)
-    elif a != b:
-        _error(a, b)
-
-
-def facts():
-    # 1
-    f = []
-    f += [
-        (0, R.NUMBER, 0),
-        (1, R.NUMBER, 1),
-        (2, R.NUMBER, 2),
-        (3, R.NUMBER, 3),
-        (4, R.NUMBER, 4),
-        (1, R.NEXT, 0),
-        (2, R.NEXT, 1),
-        (3, R.NEXT, 2),
-        (4, R.NEXT, 3),
-    ]
-    # 2
-    h = var()
-    f += [
-        (h, R.WHO, 'Englishman'),
-        (h, R.COLOR, 'red'),
-    ]
-    # 3
-    h = var()
-    f += [
-        (h, R.WHO, 'Spaniard'),
-        (h, R.OWNS, 'dog'),
-    ]
-    # 4
-    h = var()
-    f += [
-        (h, R.DRINKS, 'coffee'),
-        (h, R.COLOR, 'green'),
-    ]
-    # 5
-    h = var()
-    f += [
-        (h, R.WHO, 'Ukranian'),
-        (h, R.DRINKS, 'tea'),
-    ]
-    # 6
-    h1 = var()
-    h2 = var()
-    f += [
-        (h1, R.COLOR, 'green'),
-        (h2, R.COLOR, 'ivory'),
-        (h1, R.NEXT, h2),
-    ]
-    # 7
-    h = var()
-    f += [
-        (h, R.SMOKES, 'Old Gold'),
-        (h, R.OWNS, 'snails'),
-    ]
-    # 8
-    h = var()
-    f += [
-        (h, R.COLOR, 'yellow'),
-        (h, R.SMOKES, 'Kools'),
-    ]
-    # 9
-    h = var()
-    f += [
-        (2, R.DRINKS, 'milk'),
-    ]
-    # 10
-    h = var()
-    f += [
-        (0, R.WHO, 'Norweigan'),
-    ]
-    # 11
-    h1 = var()
-    h2 = var()
-    f += [
-        (h1, R.SMOKES, 'Chesterfields'),
-        (h2, R.OWNS, 'fox'),
-        (h1, R.NEXT, h2)
-    ]
-    # 12
-    h1 = var()
-    h2 = var()
-    f += [
-        (h1, R.SMOKES, 'Kools'),
-        (h2, R.OWNS, 'horse'),
-        (h1, R.NEXT, h2)
-    ]
-    # 13
-    h = var()
-    f += [
-        (h, R.SMOKES, 'Lucky Strike'),
-        (h, R.DRINKS, 'orange juice'),
-    ]
-    # 14
-    h = var()
-    f += [
-        (h, R.SMOKES, 'Parliaments'),
-        (h, R.DRINKS, 'Japanese'),
-    ]
-    # 15
-    h1 = var()
-    h2 = var()
-    f += [
-        (h1, R.WHO, 'Norweigan'),
-        (h2, R.COLOR, 'blue'),
-        (h1, R.NEXT, h2),
-    ]
-    return f
+    return a[0] == b[0] and unify_atom(a[2], b[2]) and unify_atom(a[1], b[1])
 
 
 def solve(facts):
-    for i, f1 in enumerate(facts):
-        for f2 in facts[i + 1:]:
+    facts = sorted(facts)
+    for i, f in enumerate(facts):
+        if f[0] == R.NEXT:
+            continue
+        for g in facts[i + 1:]:
+            if g[0] == R.NEXT:
+                continue
             try:
-                print(f1, f2)
-                unify(f1, f2)
+                # print(f, g)
+                unify(f, g)
             except ValueError:
                 pass
+    for i in range(5):
+        for f in facts:
+            unify(f, (R.NEXT, i, i + 1))
+            unify(f, (R.NEXT, i + 1, i))
 
 
 def solution():
     water = var()
     zebra = var()
-    h1 = var()
-    h2 = var()
 
-    f = facts()
-    f +=  [
-        (h1, R.DRINKS, 'water'),
-        (h1, R.WHO, water),
-        (h2, R.OWNS, 'zebra'),
-        (h2, R.WHO, zebra)
+    f, homes = facts()
+    f += [
+        (R.DRINKS, water, 'water'),
+        (R.OWNS, zebra, 'zebra'),
     ]
     solve(f)
+    for fact in f:
+        print('(%s, %s, %s)' % fact)
 
     print('SOLUTION', water, zebra)
+
+
+def facts():
+    # 1
+    facts = []
+    homes = [var(i) for i in range(5)]
+    facts += [
+        (R.RIGHT,var(0), 1),
+        (R.RIGHT,var(1), 2),
+        (R.RIGHT,var(2), 3),
+        (R.RIGHT,var(3), 4),
+    ]
+    # 2
+    h = var()
+    facts += [
+        (R.WHO, h, 'Englishman'),
+        (R.COLOR, h, 'red'),
+    ]
+    # 3
+    h = var()
+    facts += [
+        (R.WHO, h, 'Spaniard'),
+        (R.OWNS, h, 'dog'),
+    ]
+    # 4
+    h = var()
+    facts += [
+        (R.DRINKS, h, 'coffee'),
+        (R.COLOR, h, 'green'),
+    ]
+    # 5
+    h = var()
+    facts += [
+        (R.WHO, h, 'Ukranian'),
+        (R.DRINKS, h, 'tea'),
+    ]
+    # 6
+    h1 = var()
+    h2 = var()
+    facts += [
+        (R.COLOR, h1, 'ivory'),
+        (R.COLOR, h2, 'green'),
+        (R.RIGHT, h1, h2),
+    ]
+    # 7
+    h = var()
+    facts += [
+        (R.SMOKES, h, 'Old Gold'),
+        (R.OWNS, h, 'snails'),
+    ]
+    # 8
+    h = var()
+    facts += [
+        (R.COLOR, h, 'yellow'),
+        (R.SMOKES, h, 'Kools'),
+    ]
+    # 9
+    h = var()
+    facts += [
+        (R.DRINKS,var(2), 'milk'),
+    ]
+    # 10
+    h = var()
+    facts += [
+        (R.WHO,var(0), 'Norweigan'),
+    ]
+    # 11
+    h1 = var()
+    h2 = var()
+    facts += [
+        (R.SMOKES, h1, 'Chesterfields'),
+        (R.OWNS, h2, 'fox'),
+        (R.NEXT, h1, h2)
+    ]
+    # 12
+    h1 = var()
+    h2 = var()
+    facts += [
+        (R.SMOKES, h1, 'Kools'),
+        (R.OWNS, h2, 'horse'),
+        (R.NEXT, h1, h2)
+    ]
+    # 13
+    h = var()
+    facts += [
+        (R.SMOKES, h, 'Lucky Strike'),
+        (R.DRINKS, h, 'orange juice'),
+    ]
+    # 14
+    h = var()
+    facts += [
+        (R.SMOKES, h, 'Parliaments'),
+        (R.DRINKS, h, 'Japanese'),
+    ]
+    # 15
+    h1 = var()
+    h2 = var()
+    facts += [
+        (R.WHO, h1, 'Norweigan'),
+        (R.COLOR, h2, 'blue'),
+        (R.NEXT, h1, h2),
+    ]
+    return facts, homes
