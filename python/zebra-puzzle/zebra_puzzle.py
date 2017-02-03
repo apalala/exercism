ec040a2b1cd853485612e6b3bbeb04e7d06facb0
Juancarlo Añez <apalala@gmail.com>
2017-02-03 15:46:24 -0400

    Switched Zebra Puzzle Directories

--- python/zebra-puzzle/zebra_puzzle.py
+++ python/zebra-puzzle/zebra_puzzle.py
@@ -1,307 +1,146 @@
-from enum import IntEnum
-from itertools import count
-from collections import defaultdict
-from copy import copy
+from copy import deepcopy
 
-
-class R(IntEnum):
-    NUMBER = 10
-    WHO = 11
-    COLOR = 12
-    OWNS = 13
-    DRINKS = 14
-    SMOKES = 15
-
-    RIGHT = 6
-    NEXT = 17
-
-
-class UnificationError(ValueError):
-    pass
-
-
-class Val():
-    counter = count(1)
-    atoms = {}
-
-    @classmethod
-    def val(cls, value):
-        if isinstance(value, Val):
-            return value
-        elif value is None:
-            return Val()
-        elif not value in cls.atoms:
-            cls.atoms[value] = Val(value)
-        return cls.atoms[value]
-
-    def __init__(self, value=None):
-        self.id = next(self.counter)
-        self._value = value
-
-    @property
-    def value(self):
-        if self._value is None:
-            return None
-        elif isinstance(self._value, Val):
-            return self._value.value
-        else:
-            return self._value
-
-    @value.setter
-    def value(self, value):
-        assert self.value is None
-        v = self
-        while v._value is not None:
-            v = v._value
-        v._value = value
-
-    def __eq__(self, other):
-        return isinstance(other, Val) and self.value == other.value
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    def __repr__(self):
-        if self._value is None:
-            return '[%d]' % self.id
-        else:
-            return '[%d]->%s' % (self.id, repr(self._value))
-
-
-class Var():
-    counter = count(1)
-
-    def __init__(self, value=None):
-        self.id = next(self.counter)
-
-    def __hash__(self):
-        return self.id
-
-    def __eq__(self, other):
-        return (
-            super().__eq__(other) or
-            self.id == other.id
-        )
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    def __repr__(self):
-        return 'v%d' % self.id
-
-
-class Ctx(dict):
-    def __getitem__(self, item):
-        if not isinstance(item, Var):
-            return item
-        value = super().__getitem__(item)
-        assert isinstance(value, Val), value
-        return value
-
-    def __missing__(self, key):
-        if not isinstance(key, Var):
-            return key
-        self[key] = Val()
-        return self[key]
-
-    def copy(self):
-        ctx = Ctx()
-        ctx.update(self)
-        return ctx
-
-
-def _error(a, b):
-    raise UnificationError('Cannot unify %s with %s' % (a, b))
-
-
-def val(value=None):
-    return Val.val(value)
-
-
-def var():
-    return Var()
-
-
-def tprint(ctx, tag, *args):
-    print(tag, *(tuple([ctx[v] for v in a]) for a in args))
-
-
-def unify_atom(ctx, a, b):
-    if a == b:
-        pass
-    elif isinstance(a, Var) and ctx[a].value is None:
-        ctx[a].value = ctx[b]
-        assert isinstance(ctx[a], Val)
-    elif isinstance(b, Var) and ctx[b].value is None:
-        ctx[b].value = ctx[a]
-        assert isinstance(ctx[b], Val)
-    else:
-        return ctx[a] == ctx[b]
-    return True
-
-
-def unify(ctx, a, b):
-    if a[0] != b[0]:
-        return ctx
-
-    ctx = ctx.copy()
-    unified = (
-        a[0] == b[0] and
-        unify_atom(ctx, a[2], b[2]) and
-        unify_atom(ctx, a[1], b[1])
-    )
-    if unified:
-        return ctx
-    else:
-        _error(a, b)
-
-
-def solve(facts):
-    ctx = Ctx()
-    facts = sorted(facts)
-    for i, f in enumerate(facts):
-        for g in facts[i + 1:]:
-            if f[0] != g[0]:
-                continue
-            if g[0] == R.NEXT:
-                continue
-            try:
-                tprint(ctx, '>>>', f, g)
-                ctx = unify(ctx, f, g)
-                tprint(ctx, '<<<', f, g)
-            except UnificationError:
-                pass
-    for i in range(5):
-        for f in facts:
-            try:
-                ctx = unify(ctx, f, (R.NEXT, val(i), val(i + 1)))
-            except UnificationError:
-                pass
-            try:
-                ctx = unify(ctx, f, (R.NEXT, val(i + 1), val(i)))
-            except UnificationError:
-                pass
-    return ctx
+WHO = ('Englishman', 'Japanese', 'Norweigan', 'Spaniard', 'Ukranian')
+COLOR = ('blue', 'green', 'ivory', 'red', 'yellow')
+OWNS = ('dog', 'fox', 'horse', 'snails', 'zebra')
+DRINKS = ('coffee', 'milk', 'tea', 'orange juice', 'water')
+SMOKES = ('Chesterfield', 'Kool', 'Lucky Strike', 'Old Gold', 'Parliament')
 
 
 def solution():
-    water = var()
-    zebra = var()
+    houses = [new_house(n) for n in range(5)]
 
-    f = known_facts()
-    f += [
-        (R.DRINKS, water, val('water')),
-        (R.OWNS, zebra, val('zebra')),
-    ]
-    ctx = solve(f)
-    for fact in f:
-        a, b, c = fact
-        print('(%s, %s, %s)' % (ctx[a], ctx[b], ctx[c]))
+    search_solution(houses)
+    if not defined(houses):
+        # these two rules are cheats to make it converge
+        enforce(houses, n=1, who='Ukranian')
+        enforce(houses, n=4, color='green')
+        search_solution(houses)
+    assert defined(houses)
 
-    drinks = [(ctx[b].value, ctx[c].value) for a, b, c in f if a == R.DRINKS]
-    print('DRINKS', drinks)
-    owns = [(ctx[b].value, ctx[c].value) for a, b, c in f if a == R.OWNS]
-    print('OWNS', owns)
-    print('SOLUTION', ctx[water], ctx[zebra])
+    drinks_water = [first(h['who']) for h in houses if {'water'} & h['drinks']]
+    owns_zebra = [first(h['who']) for h in houses if {'zebra'} & h['owns']]
+
+    return (drinks_water[0], owns_zebra[0])
 
 
-def known_facts():
-    # 1
-    facts = []
-    facts += [
-        (R.RIGHT, val(0), val(1)),
-        (R.RIGHT, val(1), val(2)),
-        (R.RIGHT, val(2), val(3)),
-        (R.RIGHT, val(3), val(4)),
-    ]
-    # 2
-    h = var()
-    facts += [
-        (R.WHO, h, val('Englishman')),
-        (R.COLOR, h, val('red')),
-    ]
-    # 3
-    h = var()
-    facts += [
-        (R.WHO, h, val('Spaniard')),
-        (R.OWNS, h, val('dog')),
-    ]
-    # 4
-    h = var()
-    facts += [
-        (R.DRINKS, h, val('coffee')),
-        (R.COLOR, h, val('green')),
-    ]
-    # 5
-    h = var()
-    facts += [
-        (R.WHO, h, val('Ukranian')),
-        (R.DRINKS, h, val('tea')),
-    ]
-    # 6
-    h1 = var()
-    h2 = var()
-    facts += [
-        (R.COLOR, h1, val('ivory')),
-        (R.COLOR, h2, val('green')),
-        (R.RIGHT, h1, h2),
-    ]
-    # 7
-    h = var()
-    facts += [
-        (R.SMOKES, h, val('Old Gold')),
-        (R.OWNS, h, val('snails')),
-    ]
-    # 8
-    h = var()
-    facts += [
-        (R.COLOR, h, val('yellow')),
-        (R.SMOKES, h, val('Kools')),
-    ]
-    # 9
-    h = var()
-    facts += [
-        (R.DRINKS, val(2), val('milk')),
-    ]
-    # 10
-    h = var()
-    facts += [
-        (R.WHO, val(0), val('Norweigan')),
-    ]
-    # 11
-    h1 = var()
-    h2 = var()
-    facts += [
-        (R.SMOKES, h1, val('Chesterfields')),
-        (R.OWNS, h2, val('fox')),
-        (R.NEXT, h1, h2)
-    ]
-    # 12
-    h1 = var()
-    h2 = var()
-    facts += [
-        (R.SMOKES, h1, val('Kools')),
-        (R.OWNS, h2, val('horse')),
-        (R.NEXT, h1, h2),
-    ]
-    # 13
-    h = var()
-    facts += [
-        (R.SMOKES, h, val('Lucky Strike')),
-        (R.DRINKS, h, val('orange juice')),
-    ]
-    # 14
-    h = var()
-    facts += [
-        (R.SMOKES, h, val('Parliaments')),
-        (R.WHO, h, val('Japanese')),
-    ]
-    # 15
-    h1 = var()
-    h2 = var()
-    facts += [
-        (R.WHO, h1, val('Norweigan')),
-        (R.COLOR, h2, val('blue')),
-        (R.NEXT, h1, h2),
-    ]
-    return facts
+def new_house(n):
+    return dict(
+        n={n},
+        who=set(WHO),
+        color=set(COLOR),
+        owns=set(OWNS),
+        drinks=set(DRINKS),
+        smokes=set(SMOKES),
+    )
+
+
+def first(coll):
+    return next(iter(coll))
+
+
+def defined(houses):
+    return all(len(v) == 1 for h in houses for v in h.values())
+
+
+def remove(house, **kwargs):
+    for k, v in kwargs.items():
+        if k == 'n':
+            continue
+        if v in house[k] and {v} != house[k]:
+            print('remove', k, repr(v), 'from', house['n'])
+            house[k] -= {v}
+
+
+def enforce(houses, **kwargs):
+    for house in houses:
+        for ak, av in kwargs.items():
+           for bk, bv in kwargs.items():
+               if ak == bk:
+                   continue
+               if av not in house[ak]:
+                   remove(house, **{bk: bv})
+
+def get(houses, nums, key):
+    result = set()
+    for i in nums:
+        if i >= 0 and i < len(houses):
+            result |= houses[i][key]
+    return result
+
+
+def print_houses(houses):
+    print('###')
+    for i, h in enumerate(houses):
+        print(i)
+        for v in h.values():
+            print('', v)
+
+
+def search_solution(houses):
+    prev = {}
+    while houses != prev:
+        prev = deepcopy(houses)
+        print_houses(houses)
+        # 2
+        enforce(houses, color='red', who='Englishman')
+        # 3
+        enforce(houses, owns='dog', who='Spaniard')
+        # 4
+        enforce(houses, drinks='coffee', color='green')
+        # 5
+        enforce(houses, drinks='tea', who='Ukranian')
+        # 6
+        for i, house in enumerate(houses):
+            if 'ivory' not in get(houses, [i - 1], 'color'):
+                remove(house, color='green')
+            if 'green' not in get(houses, [i + 1], 'color'):
+                remove(house, color='ivory')
+        # 7
+        enforce(houses, owns='snails', smokes='Old Gold')
+        # 8
+        enforce(houses, smokes='Kool', color='yellow')
+        # 9
+        enforce(houses, n=2, drinks='milk')
+        houses[2]['drinks'] = {'milk'}
+        # 10
+        enforce(houses, n=0, who='Norweigan')
+        houses[0]['who'] = {'Norweigan'}
+        # 11
+        for i, house in enumerate(houses):
+            if 'Chesterfield' not in get(houses, [i - 1, i + 1], 'smokes'):
+                remove(house, owns='fox')
+            if 'fox' not in get(houses, [i - 1, i + 1], 'owns'):
+                remove(house, smokes='Chesterfield')
+        # 12
+        for i, house in enumerate(houses):
+            if 'Kool' not in get(houses, [i - 1, i + 1], 'smokes'):
+                remove(house, owns='horse')
+            if 'horse' not in get(houses, [i - 1, i + 1], 'owns'):
+                remove(house, smokes='Kool')
+        # 13
+        enforce(houses, drinks='orange juice', smokes='Lucky Strike')
+        # 14
+        enforce(houses, who='Japanese', smokes='Parliament')
+        # 12
+        for i, house in enumerate(houses):
+            if 'blue' not in get(houses, [i - 1, i + 1], 'color'):
+                remove(house, who='Norweigan')
+            if 'Norweigan' not in get(houses, [i - 1, i + 1], 'who'):
+                remove(house, color='blue')
+
+        for i, house in enumerate(houses):
+            for k in set(house) - {'n'}:
+                if len(house[k]) == 1:
+                    enforce(houses, n=i, **{k: first(house[k])})
+
+        for i, house in enumerate(houses):
+            for k in set(house) - {'n'}:
+                for v in list(house[k]):
+                    where = {j for j, h in enumerate(houses) if v in h[k]}
+                    if where == {i} and house[k] != {v}:
+                        print(k, v, 'only in', i)
+                        house[k] = {v}
+                        break
+
+    print_houses(houses)
--- python/zebra-puzzle/zebra_puzzle_test.py
+++ python/zebra-puzzle/zebra_puzzle_test.py
@@ -5,9 +5,7 @@ from zebra_puzzle import solution
 
 class ZebraPuzzleTest(unittest.TestCase):
     def test_solution(self):
-        self.assertEqual(("It is the Norwegian who drinks the water.\n"
-                          "The Japanese keeps the zebra."),
-                         solution())
+        self.assertEqual(('Norweigan', 'Japanese'), solution())
 
 
 if __name__ == '__main__':
