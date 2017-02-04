4e1fd1e1f976db78060a1963fee92d9479583727
Juancarlo Añez <apalala@gmail.com>
2017-02-03 21:34:11 -0400

    Refactored the solution by @BrianLusina http://exercism.io/submissions/a824ec60503e469db9e126a83f5e3cfa

--- python/zebra-puzzle/zebra_puzzle.py
+++ python/zebra-puzzle/zebra_puzzle.py
@@ -1,146 +1,66 @@
-from copy import deepcopy
+# from the solution by @BrianLusina
+# http://exercism.io/submissions/a824ec60503e469db9e126a83f5e3cfa
 
-WHO = ('Englishman', 'Japanese', 'Norweigan', 'Spaniard', 'Ukranian')
-COLOR = ('blue', 'green', 'ivory', 'red', 'yellow')
-OWNS = ('dog', 'fox', 'horse', 'snails', 'zebra')
-DRINKS = ('coffee', 'milk', 'tea', 'orange juice', 'water')
-SMOKES = ('Chesterfield', 'Kool', 'Lucky Strike', 'Old Gold', 'Parliament')
+from collections import namedtuple
+from itertools import permutations
+
+
+Color = namedtuple('Color', 'red green ivory yellow blue')
+Who = namedtuple('Who', 'englishman spaniard ukranian japanese norwegian')
+Drink = namedtuple('Drinks', 'coffee tea milk orange_juice, water')
+Smokes = namedtuple('Smokes', 'old_gold kools chesterfields lucky_strike parliaments')
+Pet = namedtuple('Pet', 'dog snails fox horse zebra')
 
 
 def solution():
-    houses = [new_house(n) for n in range(5)]
+    houses = range(5)
+    first, second, middle, third, last = houses
 
-    search_solution(houses)
-    if not defined(houses):
-        # these two rules are cheats to make it converge
-        enforce(houses, n=1, who='Ukranian')
-        enforce(houses, n=4, color='green')
-        search_solution(houses)
-    assert defined(houses)
+    orderings = list(tuple(p) for p in permutations(houses))
+    result = [
+        (color, who, drink, smokes, pet)
+        for color in (Color(*c) for c in orderings)
+        if (
+            _right_of(color.green, color.ivory)
+        )
+        for who in (Who(*w) for w in orderings)
+        if (
+            who.englishman == color.red and
+            who.norwegian == first and
+            _next_to(who.norwegian, color.blue)
+        )
+        for drink in (Drink(*d) for d in orderings)
+        if (
+            drink.coffee == color.green and
+            who.ukranian == drink.tea and
+            drink.milk == middle
+        )
+        for smokes in (Smokes(*s) for s in orderings)
+        if (
+            smokes.kools == color.yellow and
+            smokes.lucky_strike == drink.orange_juice and
+            who.japanese == smokes.parliaments
+        )
+        for pet in (Pet(*p) for p in orderings)
+        if (
+            who.spaniard == pet.dog and
+            smokes.old_gold == pet.snails and
+            _next_to(smokes.chesterfields, pet.fox) and
+            _next_to(smokes.kools, pet.horse)
+        )
+    ]
+    color, who, drink, smokes, pet = result[0]
 
-    drinks_water = [first(h['who']) for h in houses if {'water'} & h['drinks']]
-    owns_zebra = [first(h['who']) for h in houses if {'zebra'} & h['owns']]
-
-    return (drinks_water[0], owns_zebra[0])
-
-
-def new_house(n):
-    return dict(
-        n={n},
-        who=set(WHO),
-        color=set(COLOR),
-        owns=set(OWNS),
-        drinks=set(DRINKS),
-        smokes=set(SMOKES),
-    )
-
-
-def first(coll):
-    return next(iter(coll))
-
-
-def defined(houses):
-    return all(len(v) == 1 for h in houses for v in h.values())
-
-
-def remove(house, **kwargs):
-    for k, v in kwargs.items():
-        if k == 'n':
-            continue
-        if v in house[k] and {v} != house[k]:
-            print('remove', k, repr(v), 'from', house['n'])
-            house[k] -= {v}
-
-
-def enforce(houses, **kwargs):
-    for house in houses:
-        for ak, av in kwargs.items():
-           for bk, bv in kwargs.items():
-               if ak == bk:
-                   continue
-               if av not in house[ak]:
-                   remove(house, **{bk: bv})
-
-def get(houses, nums, key):
-    result = set()
-    for i in nums:
-        if i >= 0 and i < len(houses):
-            result |= houses[i][key]
-    return result
+    who_names = {value: name.capitalize() for name, value in who._asdict().items()}
+    return (
+        "It is the {} who drinks the water.\n"
+        "The {} keeps the zebra."
+    ).format(who_names[drink.water], who_names[pet.zebra])
 
 
-def print_houses(houses):
-    print('###')
-    for i, h in enumerate(houses):
-        print(i)
-        for v in h.values():
-            print('', v)
+def _right_of(x, y):
+    return x - y == 1
 
 
-def search_solution(houses):
-    prev = {}
-    while houses != prev:
-        prev = deepcopy(houses)
-        print_houses(houses)
-        # 2
-        enforce(houses, color='red', who='Englishman')
-        # 3
-        enforce(houses, owns='dog', who='Spaniard')
-        # 4
-        enforce(houses, drinks='coffee', color='green')
-        # 5
-        enforce(houses, drinks='tea', who='Ukranian')
-        # 6
-        for i, house in enumerate(houses):
-            if 'ivory' not in get(houses, [i - 1], 'color'):
-                remove(house, color='green')
-            if 'green' not in get(houses, [i + 1], 'color'):
-                remove(house, color='ivory')
-        # 7
-        enforce(houses, owns='snails', smokes='Old Gold')
-        # 8
-        enforce(houses, smokes='Kool', color='yellow')
-        # 9
-        enforce(houses, n=2, drinks='milk')
-        houses[2]['drinks'] = {'milk'}
-        # 10
-        enforce(houses, n=0, who='Norweigan')
-        houses[0]['who'] = {'Norweigan'}
-        # 11
-        for i, house in enumerate(houses):
-            if 'Chesterfield' not in get(houses, [i - 1, i + 1], 'smokes'):
-                remove(house, owns='fox')
-            if 'fox' not in get(houses, [i - 1, i + 1], 'owns'):
-                remove(house, smokes='Chesterfield')
-        # 12
-        for i, house in enumerate(houses):
-            if 'Kool' not in get(houses, [i - 1, i + 1], 'smokes'):
-                remove(house, owns='horse')
-            if 'horse' not in get(houses, [i - 1, i + 1], 'owns'):
-                remove(house, smokes='Kool')
-        # 13
-        enforce(houses, drinks='orange juice', smokes='Lucky Strike')
-        # 14
-        enforce(houses, who='Japanese', smokes='Parliament')
-        # 12
-        for i, house in enumerate(houses):
-            if 'blue' not in get(houses, [i - 1, i + 1], 'color'):
-                remove(house, who='Norweigan')
-            if 'Norweigan' not in get(houses, [i - 1, i + 1], 'who'):
-                remove(house, color='blue')
-
-        for i, house in enumerate(houses):
-            for k in set(house) - {'n'}:
-                if len(house[k]) == 1:
-                    enforce(houses, n=i, **{k: first(house[k])})
-
-        for i, house in enumerate(houses):
-            for k in set(house) - {'n'}:
-                for v in list(house[k]):
-                    where = {j for j, h in enumerate(houses) if v in h[k]}
-                    if where == {i} and house[k] != {v}:
-                        print(k, v, 'only in', i)
-                        house[k] = {v}
-                        break
-
-    print_houses(houses)
+def _next_to(x, y):
+    return abs(x - y) == 1
