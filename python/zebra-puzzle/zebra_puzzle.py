815eb50180c4634bb851a00d8261d37f45721dc5
Juancarlo Añez <apalala@gmail.com>
2017-02-03 22:08:29 -0400

    Zebra Puzzle: more refactorings

--- python/zebra-puzzle/zebra_puzzle.py
+++ python/zebra-puzzle/zebra_puzzle.py
@@ -6,28 +6,31 @@ from itertools import permutations
 
 
 Color = namedtuple('Color', 'red green ivory yellow blue')
-Who = namedtuple('Who', 'englishman spaniard ukranian japanese norwegian')
+Person = namedtuple('Person', 'englishman spaniard ukranian japanese norwegian')
 Drink = namedtuple('Drinks', 'coffee tea milk orange_juice, water')
 Smokes = namedtuple('Smokes', 'old_gold kools chesterfields lucky_strike parliaments')
 Pet = namedtuple('Pet', 'dog snails fox horse zebra')
-Houses = namedtuple('Houses', 'color who drink smokes pet')
+Houses = namedtuple('Houses', 'color person drink smokes pet')
 
 
 def solution():
     result = _find_solutions()[0]
-    who_names = {
+    persons = {
         value: name.capitalize()
-        for name, value in result.who._asdict().items()}
+        for name, value in result.person._asdict().items()}
     return (
         "It is the {} who drinks the water.\n"
         "The {} keeps the zebra."
     ).format(
-        who_names[result.drink.water],
-        who_names[result.pet.zebra]
+        persons[result.drink.water],
+        persons[result.pet.zebra]
     )
 
 
 def _find_solutions():
+    def is_same_house(x, y):
+        return x == y
+
     def is_right_of(x, y):
         return x - y == 1
 
@@ -38,36 +41,35 @@ def _find_solutions():
     first, second, middle, third, last = house_numbers
 
     orderings = list(tuple(p) for p in permutations(house_numbers))
-    houses = [
-        Houses(color=color, who=who, drink=drink, smokes=smokes, pet=pet)
+    return [
+        Houses(color=color, person=person, drink=drink, smokes=smokes, pet=pet)
         for color in (Color(*c) for c in orderings)
         if (
             is_right_of(color.green, color.ivory)
         )
-        for who in (Who(*w) for w in orderings)
+        for person in (Person(*w) for w in orderings)
         if (
-            who.englishman == color.red and
-            who.norwegian == first and
-            is_next_to(who.norwegian, color.blue)
+            is_same_house(person.englishman, color.red) and
+            is_same_house(person.norwegian, first) and
+            is_next_to(person.norwegian, color.blue)
         )
         for drink in (Drink(*d) for d in orderings)
         if (
-            drink.coffee == color.green and
-            who.ukranian == drink.tea and
-            drink.milk == middle
+            is_same_house(drink.coffee, color.green) and
+            is_same_house(person.ukranian, drink.tea) and
+            is_same_house(drink.milk, middle)
         )
         for smokes in (Smokes(*s) for s in orderings)
         if (
-            smokes.kools == color.yellow and
-            smokes.lucky_strike == drink.orange_juice and
-            who.japanese == smokes.parliaments
+            is_same_house(smokes.kools, color.yellow) and
+            is_same_house(smokes.lucky_strike, drink.orange_juice) and
+            is_same_house(person.japanese, smokes.parliaments)
         )
         for pet in (Pet(*p) for p in orderings)
         if (
-            who.spaniard == pet.dog and
-            smokes.old_gold == pet.snails and
+            is_same_house(person.spaniard, pet.dog) and
+            is_same_house(smokes.old_gold, pet.snails) and
             is_next_to(smokes.chesterfields, pet.fox) and
             is_next_to(smokes.kools, pet.horse)
         )
     ]
-    return houses
